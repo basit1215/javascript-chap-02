7c5e840753eb67f0ff0bb5a6bf0c230b0e101b15
basit1215 <haizabdulbasit225@gmail.com>
2024-02-29 22:41:47 +0500

    updated

---
+++ app.js
@@ -0,0 +1,29 @@
+var userName = "Jhone Doe";
+
+alert(userName);
+
+var age= "15 years old";
+
+alert(age);
+
+ var message= "Certified Mobile Application Development";
+
+alert(message);
+
+ var food= "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+alert(food);
+
+ var email= "My email address is example@example.com";
+
+ alert(email);
+
+ var javascriptBook= "I am trying to learn from the Book A smarter way to learn Javascript";
+
+ alert(javascriptBook);
+
+ var symbol="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+ alert(symbol);
+
+ 
